8bc9d9eebf63f7bbded232591a40c646daee9901
Toyin Balogun <balogunt302@gmail.com>
2021-12-26 17:26:57 -0500

    solved merge_intervals leetcode medium difficulty probmlem

--- leetcode-questions/merge-intervals.py
+++ leetcode-questions/merge-intervals.py
@@ -12,4 +12,21 @@
 # Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 
 def merge_intervals(intervals):
-    return 1
+    intervals.sort()
+    result = [intervals[0]] #[[1,3],...]
+
+    for i in intervals[1:]: #[[2,6],[8,10],[15,18]]
+        if result[-1][1] < i[0]:
+            result.append(i)
+        else:
+           result[-1][1] = max(result[-1][1], i[1]) 
+    return result
+
+#TESTS  
+intervals1 = [[1,3],[2,6],[8,10],[15,18]]
+intervals2 = [[1,4],[4,5]]
+intervals3 = [[1,4],[2,3]]
+
+print(merge_intervals(intervals1))
+print(merge_intervals(intervals2))
+print(merge_intervals(intervals3))
